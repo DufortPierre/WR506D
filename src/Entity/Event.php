cc63642cda0e50abc794243fbb5f95669448bcc7
DufortPierre <pierre.dufort@etudiant.univ-reims.fr>
2026-01-11 15:29:00 +0100

    Remove empty DemoFixtures file

---
+++ src/Entity/Event.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Movies = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $ReleaseDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMovies(): ?string
+    {
+        return $this->Movies;
+    }
+
+    public function setMovies(string $Movies): static
+    {
+        $this->Movies = $Movies;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?int
+    {
+        return $this->description;
+    }
+    public function setDescription(int $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTime
+    {
+        return $this->ReleaseDate;
+    }
+
+    public function setReleaseDate(\DateTime $ReleaseDate): static
+    {
+        $this->ReleaseDate = $ReleaseDate;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
