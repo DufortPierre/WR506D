0bb192c5533d59cd8042b4464428cdef2bf61120
DufortPierre <pierre.dufort@etudiant.univ-reims.fr>
2026-01-11 20:25:18 +0100

    Add CreateUserCommand to easily create users

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:user:create',
+    description: 'Create a new user with email and password',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'User email address')
+            ->addArgument('password', InputArgument::REQUIRED, 'User password')
+            ->addOption('role', 'r', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'User roles', ['ROLE_USER'])
+            ->addOption('firstname', 'f', InputOption::VALUE_OPTIONAL, 'User firstname')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $roles = $input->getOption('role');
+        $firstname = $input->getOption('firstname');
+
+        // Vérifier si l'utilisateur existe déjà
+        $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        if ($existingUser) {
+            $io->error("User with email '{$email}' already exists!");
+            return Command::FAILURE;
+        }
+
+        // Créer le nouvel utilisateur
+        $user = new User();
+        $user->setEmail($email);
+        $user->setFirstname($firstname);
+        $user->setRoles($roles);
+
+        // Hasher le mot de passe
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        // Sauvegarder
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success("User created successfully!");
+        $io->table(
+            ['Property', 'Value'],
+            [
+                ['ID', (string) $user->getId()],
+                ['Email', $user->getEmail()],
+                ['Firstname', $user->getFirstname() ?? 'N/A'],
+                ['Roles', implode(', ', $user->getRoles())],
+            ]
+        );
+
+        $io->note("You can now login with:\nEmail: {$email}\nPassword: {$password}");
+
+        return Command::SUCCESS;
+    }
+}
